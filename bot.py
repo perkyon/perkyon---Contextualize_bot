dab5aef46e069de8542909e8423e268382bb56f4
Илья Фоминцев <fominsevil@gmail.com>
2023-04-02 02:38:55 +0300

    Main_1

--- bot.py
+++ bot.py
@@ -1,21 +1,41 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, filters
-from telegram import Chat as TGChat
+import logging
+import openai
+from telegram import Update, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет, я бот!")
+# Укажите ваш токен для OpenAI
+openai.api_key = "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"
 
+# Укажите токен вашего телеграм-бота
+TELEGRAM_BOT_TOKEN = "6007270444:AAHREzJ4nfr-GRIOd7k9wi4q5tFq8scEUuM"
 
-def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text("Привет! Я бот, который объясняет значение контекста с помощью ChatGPT. Пиши свои вопросы, и я постараюсь помочь!")
 
+def handle_message(update: Update, context: CallbackContext):
+    user_text = update.message.text
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=f"Объясните значение следующего контекста: {user_text}",
+        max_tokens=50,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+    chatgpt_response = response.choices[0].text.strip()
+    update.message.reply_text(chatgpt_response)
 
 def main():
-    updater = Updater(token='6007270444:AAHREzJ4nfr-GRIOd7k9wi4q5tFq8scEUuM', use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    logging.basicConfig(level=logging.INFO)
+
+    updater = Updater(token=TELEGRAM_BOT_TOKEN)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(MessageHandler(Filters.text, handle_message))
+
     updater.start_polling()
+    updater.idle()
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
