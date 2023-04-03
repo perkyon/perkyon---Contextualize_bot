9fc72c12afed2d9f2eb8cccff6c40aedc082921e
Илья Фоминцев <fominsevil@gmail.com>
2023-04-03 14:49:51 +0300

    main_2

--- bot.py
+++ bot.py
@@ -1,41 +1,43 @@
-import logging
 import openai
-from telegram import Update, Filters
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, ApplicationBuilder, filters
 
-# Укажите ваш токен для OpenAI
-openai.api_key = "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"
+# Здесь введите токен вашего бота
+TELEGRAM_BOT_TOKEN = '6007270444:AAHREzJ4nfr-GRIOd7k9wi4q5tFq8scEUuM'
 
-# Укажите токен вашего телеграм-бота
-TELEGRAM_BOT_TOKEN = "6007270444:AAHREzJ4nfr-GRIOd7k9wi4q5tFq8scEUuM"
+# Здесь введите ваш API-ключ OpenAI
+OPENAI_API_KEY = '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'
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Привет! Я бот, который объясняет значение контекста с помощью ChatGPT. Пиши свои вопросы, и я постараюсь помочь!")
+# Инициализируем подключение к API OpenAI
+openai.api_key = OPENAI_API_KEY
 
-def handle_message(update: Update, context: CallbackContext):
-    user_text = update.message.text
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=f"Объясните значение следующего контекста: {user_text}",
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-    chatgpt_response = response.choices[0].text.strip()
-    update.message.reply_text(chatgpt_response)
 
-def main():
-    logging.basicConfig(level=logging.INFO)
+# Функция-обработчик текстовых сообщений
+async def reply_message(update: Updater, context: CallbackContext):
+    # Получаем текст сообщения, отправленного пользователем
+    user_message = update.message.text
 
-    updater = Updater(token=TELEGRAM_BOT_TOKEN)
-    dp = updater.dispatcher
+    # Задаем параметры для запроса к API OpenAI
+    prompt = f"Explain '{user_message}' in simple terms"
+    model = 'text-davinci-002'
+    temperature = 0.5
+    max_tokens = 100
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
+    # Получаем ответ от API OpenAI
+    response = openai.Completion.create(engine=model, prompt=prompt, temperature=temperature, max_tokens=max_tokens)
+    bot_reply = response.choices[0].text
 
-    updater.start_polling()
-    updater.idle()
+    # Отправляем ответ от бота
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=bot_reply)
 
-if __name__ == "__main__":
-    main()
+# Функция-обработчик команды /start
+async def start(update: Updater, context: CallbackContext):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот.")
+
+# Функция для запуска бота
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    print("Успех")
+    start_handler = CommandHandler('Start', start)
+    application.add_handler(start_handler)
+    application.run_polling()
